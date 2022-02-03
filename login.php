9e782df70322375d54ab79830eeaf6aaf6f3fe0e
Lucas Garcia <lucasgarcia561@gmail.com>
2022-02-03 11:52:56 -0300

    teste de conexão

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
     $email = $_POST['email'];
     $senha = $_POST ['senha'];
-
+// conexao BD//
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -9,9 +9,19 @@
 try {
   $conn = new PDO("mysql:host=$servername;dbname=sistema", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Conectado com sucesso";
+ // echo "Conectado com sucesso";
+  $stmt = $conn->prepare("SELECT codigo FROM tbusuario WHERE email=:email AND senha=:senha);
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
 } catch(PDOException $e) {
   echo "Falha na conexão" . $e->getMessage();
 }
+$conn = null;
+//verificar dados do banco de dados//
 
 ?>
