06a06a9e280ae41b06b6b901cc0732658b9b1693
Lucas Garcia <lucasgarcia561@gmail.com>
2022-02-03 14:37:32 -0300

    autenticando o usuário

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+    if (count($_POST) > 0) {
+//pegando valores do formulário
     $email = $_POST['email'];
     $senha = $_POST ['senha'];
 // conexao BD//
@@ -9,19 +11,29 @@
 try {
   $conn = new PDO("mysql:host=$servername;dbname=sistema", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- // echo "Conectado com sucesso";
-  $stmt = $conn->prepare("SELECT codigo FROM tbusuario WHERE email=:email AND senha=:senha);
+//echo "Conectado com sucesso";//
+
+//verificando dados do usuário//
+  $stmt = $conn->prepare("SELECT codigo FROM tbusuario WHERE email=:email AND senha=md5(:senha)");
+  $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+  $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
   $stmt->execute();
 
-  // set the resulting array to associative
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
+  $result = $stmt->fetchALL();
+  $qtd_usuarios = count($result);
+  if($qtd_usuarios == 1){
+    //TODO substituindo o redicionamento//
+    $resultado["msg"] =  "Usuário encontrado!";
+    $resultado["cod"] = 1;
+  } else if ($qtd_usuarios == 0){
+    $resultado["msg"] =  "E-mail e senha não conferem!";
+    $resultado["cod"] = 0;
   }
-} catch(PDOException $e) {
-  echo "Falha na conexão" . $e->getMessage();
-}
-$conn = null;
-//verificar dados do banco de dados//
-
+  }
+      catch(PDOException $e) {
+        echo "Falha na conexão" . $e->getMessage();
+    }
+    $conn = null;
+}  
+   include("index.php");
 ?>
